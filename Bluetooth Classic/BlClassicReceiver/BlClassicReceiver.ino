af1b7cdae2be6cd2d6b6a05a2ddf5b3c153d66d6
Nimeth Menuka <nimethm5@gmail.com>
2024-03-31 21:54:51 +0530

    small changes

--- Bluetooth Classic/BlClassicReceiver/BlClassicReceiver.ino
+++ Bluetooth Classic/BlClassicReceiver/BlClassicReceiver.ino
@@ -29,16 +29,16 @@ unsigned long timerDelay = 3000;
 void setup() {
     //OLED display setup
   // SSD1306_SWITCHCAPVCC = generate display voltage from 3.3V internally
-  if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) { // Address 0x3C for 128x32
+  if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3D)) { // Address 0x3C for 128x32
     Serial.println(F("SSD1306 allocation failed"));
     for(;;); // Don't proceed, loop forever
   }
 
   display.clearDisplay();
-  display.setTextSize(1);
+  display.setTextSize(2);
   display.setTextColor(WHITE,0);
   display.setCursor(0,25);
-  display.print("Bluetooth Classic Client");
+  display.print("BL Classic Client");
   display.display();
 
   Serial.begin(115200);
